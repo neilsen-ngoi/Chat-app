68b517eafda01ca4ca2d948518860059ab2e2e39
neilsen-ngoi <kenisu222@gmail.com>
2023-11-06 15:58:34 +1300

    update app name

--- app/hooks/useOtherUser.ts
+++ app/hooks/useOtherUser.ts
@@ -11,13 +11,16 @@ const useOtherUser = (
         users: User[]
       }
 ) => {
+
   const session = useSession()
+
   const otherUser = useMemo(() => {
     const currentUserEmail = session?.data?.user?.email
+
     const otherUser = conversation.users.filter(
-      (user) => user.email !== currentUserEmail
-    )
+      (user) => user.email !== currentUserEmail)
     return otherUser[0]
+    
   }, [session?.data?.user?.email, conversation.users])
   return otherUser
 }
