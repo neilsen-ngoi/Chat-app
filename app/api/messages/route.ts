c59e35e2683ebb8fa14659361937601475c3ad67
neilsen-ngoi <kenisu222@gmail.com>
2023-11-20 11:46:20 +1300

    add pusher to messages route

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,8 @@
-import getCurrentUser from '@/app/actions/getCurrentUser'
 import { NextResponse } from 'next/server'
+
+import getCurrentUser from '@/app/actions/getCurrentUser'
 import prisma from '@/app/libs/prismadb'
+import { pusherServer } from '@/app/libs/Pusher'
 
 export async function POST(request: Request) {
   try {
@@ -51,6 +53,17 @@ export async function POST(request: Request) {
           },
         },
       },
+    })
+
+    await pusherServer.trigger(conversationId, 'messages:new', newMessage)
+    const lastMessage =
+      updatedConversation.messages[updatedConversation.messages.length - 1]
+
+    updatedConversation.users.map((user) => {
+      pusherServer.trigger(user.email!, 'conversationUpdate', {
+        id: conversationId,
+        messages: [lastMessage],
+      })
     })
 
     return NextResponse.json(newMessage)
